620019a344f91736b839170b5345656bd399ccb4
Tuan Anh <anh.pngt@gmail.com>
2026-04-02 00:21:43 +0700

    update

---
+++ BTTH02_25730098_PhamNguyenTuanAnh/6.h
@@ -0,0 +1,23 @@
+// Mã số sinh viên: 25730098
+// Họ và tên: Phạm Nguyễn Tuấn Anh
+// Ngày sinh: 15/05/1996
+// Lớp: CN1.K2025.2.CNTT
+
+// 6. Cài đặt lớp String để biểu diễn khái niệm chuỗi ký tự với các phương thức thiết
+// lập và hủy bỏ, các hàm thành phần xác định chiều dài chuỗi, nối hai chuỗi, đảo
+// chuỗi, chuyển chuỗi hoa/thường, nhập và xuất chuỗi. Viết chương trình minh họa
+// các chức năng đã cài đặt của lớp String.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class cString
+{
+public:
+    cString();
+    cString(string sString);
+};
